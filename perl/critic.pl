d154aca3af1845e13c2f334a1b4af7ff615f89a7
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2007-02-19 07:05:53 +0000

    Added support for configurable severity level

--- perl/critic.pl
+++ perl/critic.pl
@@ -16,7 +16,6 @@ use Carp;
 
 our $TT_INCLUDE = '/var/www/vhosts/perlcritic/tt2';
 our $TT = Template->new( {INCLUDE_PATH => $TT_INCLUDE} );
-our $PC = Perl::Critic->new( -severity => 1 );
 our $HL = create_highlighter();
 
 #-----------------------------------------------------------------------------
@@ -25,9 +24,10 @@ if ( http('HTTP_USER_AGENT') =~ m{ (?: mozilla|msie ) }imx ) {
     eval {
 	my $source_fh           = upload('code_file');
 	my $source_path         = param('code_file');
+        my $severity            = param('severity');
 	my ($raw, $cooked)      = load_source_code( $HL, $source_fh );
 	my $code_frame_url      = generate_code_frame( $TT, $cooked );
-	my @violations          = critique_source_code( $PC, $raw );
+	my @violations          = critique_source_code( $severity, $raw );
 	my $critique_frame_url  = generate_critique_frame( $TT, $code_frame_url, @violations );
 	my $status              = render_page( $TT, $source_path, $code_frame_url, $critique_frame_url );
     };
@@ -39,7 +39,7 @@ if ( http('HTTP_USER_AGENT') =~ m{ (?: mozilla|msie ) }imx ) {
 
 else {
   my $raw                   = \do{  local $/ = undef; <STDIN> };
-  my @violations            = critique_source_code( $PC, $raw );
+  my @violations            = critique_source_code( 1, $raw );
   print header, @violations;
 }
 
@@ -58,8 +58,9 @@ sub render_page {
 #-----------------------------------------------------------------------------
 
 sub critique_source_code {
-    my ($PC, $source_ref) = @_;
-    my @viols = $PC->critique( $source_ref );
+    my ($severity, $source_ref) = @_;
+    my $critic = Perl::Critic->new( -severity => $severity );
+    my @viols = $critic->critique( $source_ref );
     return @viols;
 }
 
